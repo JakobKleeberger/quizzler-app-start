2f934a11b4246a2a307b2ab2304724e4c5209ddb
confusedalex <alex@confusedalex.dev>
2025-01-28 10:02:07 +0100

    refactor: simplify flake

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+  };
+
+  outputs =
+    {
+      systems,
+      nixpkgs,
+      ...
+    }:
+    let
+      eachSystem = f: nixpkgs.lib.genAttrs (import systems) (system: f nixpkgs.legacyPackages.${system});
+    in
+    {
+      devShells = eachSystem (pkgs: {
+        default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (pkgs.python312.withPackages (
+              python-pkgs: with python-pkgs; [
+                requests
+                tkinter
+              ]
+            ))
+          ];
+        };
+      });
+    };
+}
